075cf91408c6e0936cee20056b045c58006e284c
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2023-02-05 12:47:06 -0300

    deleted model directory and changed load_model function parameter to assets' model.

--- api/app.py
+++ api/app.py
@@ -3,25 +3,13 @@ from flask import Flask, jsonify, request
 from tensorflow import keras
 # import functools
 # from tensorflow.keras.models import load_model
+import keras.models
 import numpy as np
 from functions import create_dataset, moving_average
 from sklearn.preprocessing import MinMaxScaler
-# import os
 
 app = Flask(__name__)
-model = keras.models.load_model("../cdipredic/model/model.h5")
-
-# def moving_average(x, w):
-#     return np.convolve(x, np.ones(w), 'valid') / w
-
-# def create_dataset(dataset, look_back=1):
-#   dataX, dataY = [], []
-#   for i in range(len(dataset)-look_back-1):
-#     a = dataset[i:(i+look_back), 0]
-#     dataX.append(a)
-#     dataY.append(dataset[i + look_back, 0])
-#   return np.array(dataX), np.array(dataY)
-
+model = keras.models.load_model("../assets/model.h5")
 
 @app.route("/api", methods=["POST"])
 def login():
