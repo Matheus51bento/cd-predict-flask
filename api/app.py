3bcd9660a4542ebb519024cb1d71a90fa2232e86
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2023-02-05 17:16:00 -0300

    moved the functions back to their original places.

--- api/app.py
+++ api/app.py
@@ -8,6 +8,17 @@ from sklearn.preprocessing import MinMaxScaler
 app = Flask(__name__)
 model = keras.models.load_model("../assets/model.h5")
 
+def moving_average(x, w):
+    return np.convolve(x, np.ones(w), 'valid') / w
+
+def create_dataset(dataset, look_back=1):
+  dataX, dataY = [], []
+  for i in range(len(dataset)-look_back-1):
+    a = dataset[i:(i+look_back), 0]
+    dataX.append(a)
+    dataY.append(dataset[i + look_back, 0])
+  return np.array(dataX), np.array(dataY)
+
 @app.route("/api", methods=["POST"])
 def login():
     # dinheiro = request.args.get('dinheiro')
